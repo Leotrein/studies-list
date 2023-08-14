cd5f7b3459ecea520daebb919d35fb27f7d44596
Leonardo Trein <leonardodt@bempromotora.com.br>
2023-08-14 16:34:17 -0300

    initial structure

---
+++ src/pages/App.tsx
@@ -0,0 +1,14 @@
+import Formulario from "../components/Form";
+import Lista from "../components/List";
+import "./style.scss";
+
+function App() {
+  return (
+    <div className="AppStyle">
+      <Formulario />
+      <Lista />
+    </div>
+  );
+}
+
+export default App;
